a62177c0ec0bfdd74367c5a5f10752c9e5508bf6
igustavo11 <131629449+igustavo11@users.noreply.github.com>
2024-10-16 22:51:29 -0300

    update2

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,13 @@
-import './home.css'
+import './home.css';
 
 function Home() {
     return (
-        
-        <div>
-        
+        <div className="home-container">
+            <div className="content-center">
+                <h1 className="home-title">Welcome to Prosperium</h1>
+                <button className="learn-more-btn">Learn More</button>
+            </div>
         </div>
-        
-       
     );
 }
 
