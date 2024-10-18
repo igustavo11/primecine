4d45521400204108492f2333b4180932562057a6
Gustavo <gustavobrun1224@gmail.com>
2024-10-18 09:16:12 -0300

    update header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,22 +2,10 @@ import './header.css';
 import { Link, useLocation } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 
-import homeImage from '../../../src/teste1.jpg'; 
-import aboutImage from '../../../src/teste2.jpg'; 
-
 function Header() {
     const [scrolling, setScrolling] = useState(false);
-    const location = useLocation();
-    const [backgroundImage, setBackgroundImage] = useState(homeImage);
     const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-   
-    const preloadImages = (images) => {
-        images.forEach((src) => {
-            const img = new Image();
-            img.src = src;
-        });
-    };
+    const location = useLocation();
 
     const handleScroll = () => {
         setScrolling(window.scrollY > 50);
@@ -31,25 +19,6 @@ function Header() {
         setIsMenuOpen(false);
     };
 
-
-    useEffect(() => {
-        const imagesToPreload = [homeImage, aboutImage];
-        preloadImages(imagesToPreload);
-    }, []);
-
-    useEffect(() => {
-        const imageMap = {
-            '/': homeImage,
-            '/about': aboutImage,
-            '/team': aboutImage,
-            '/contact': homeImage,
-        };
-
-        const newImage = imageMap[location.pathname] || homeImage;
-
-        setBackgroundImage(newImage);
-    }, [location.pathname]);
-
     useEffect(() => {
         window.addEventListener('scroll', handleScroll);
         return () => {
@@ -58,10 +27,9 @@ function Header() {
     }, []);
 
     return (
-        <header className={`header ${scrolling ? 'scrolled' : ''}`} style={{ backgroundImage: `url(${backgroundImage})` }}>
+        <header className={`header ${scrolling ? 'scrolled' : ''} ${location.pathname === '/' ? 'home' : location.pathname.slice(1)}`}>
             <nav className={`navbar ${scrolling ? 'scrolled' : ''}`}>
-
-            <div className={`nav-links ${isMenuOpen ? 'menu-open' : ''}`}>
+                <div className={`nav-links ${isMenuOpen ? 'menu-open' : ''}`}>
                     <span className="close-menu" onClick={closeMenu}>X</span>
                     <Link className="home" to="/" onClick={closeMenu}>Home</Link>
                     <Link className="about" to="/about" onClick={closeMenu}>About</Link>
