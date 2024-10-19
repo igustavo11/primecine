f5e4a028df8cabe1af12ee0235677c6a41a4aedc
Fernanda <fernanvipo7@gmail.com>
2024-10-18 23:56:00 -0300

    create about and component

---
+++ src/components/AboutCard/index.js
@@ -0,0 +1,32 @@
+import './aboutcard.css'
+
+function AboutCard(){
+    const about = [
+        {
+            id: 1,
+            photo:'https://images.squarespace-cdn.com/content/v1/631349e6c645d416c5f05d60/eeabbe03-e99b-4aac-bb15-7cc65ad61e69/3.png?format=750w',
+            title:'MISSION',
+            paragraph:'Generate attractive returns on equity for our partners and investors based on asset appreciation, all with a mindset of climate awareness through efficiency.',
+        },
+        {
+            id:2,
+            photo:'https://images.squarespace-cdn.com/content/v1/631349e6c645d416c5f05d60/e5d10360-aaf6-4aa6-bb80-a53ecdcbd2b4/4.png?format=750w',
+            title:'STRATEGY',
+            paragraph:'Create homes with sophisticated, modern designs and processes. We leverage materials and products that have demonstrated energy efficiency and durability.',
+        }
+    ]
+
+    return(
+        <div className="cards">
+         {about.map(post => (
+            <div key={post.id}>
+                <img src={post.photo}/>
+                <h3>{post.title}</h3>
+                <p>{post.paragraph}</p>
+            </div>
+        ))}
+
+        </div>
+    );
+};
+export default AboutCard;
--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,12 +1,18 @@
 
 import './about.css';
+import AboutCard from '../../components/AboutCard';
 
 
 
 function About(){
     return(
-        <div className="">
-          <h1>Oi</h1>
+        <div className="about-container">
+          <div className="transition-paragraph">
+            <p>Shape Equity Partners is a boutique investment firm founded in 2016. We identify, fund and execute efficient real estate development projects. We have consistently demonstrated our ability to identify unique opportunities and successfully execute our strategies in spite of ever-changing market conditions on a local and global scale.</p>
+            <hr/>
+          </div>
+          <AboutCard></AboutCard>
+
         </div>
     
     )
