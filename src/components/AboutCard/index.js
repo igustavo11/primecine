15d65cf31ec9eb9fbf023d8ed1cfc6b3be92ae74
Fernanda <fernanvipo7@gmail.com>
2024-10-20 22:43:56 -0300

    update on about page and aboutcards component

--- src/components/AboutCard/index.js
+++ src/components/AboutCard/index.js
@@ -13,15 +13,15 @@ function AboutCard(){
             photo:'https://images.squarespace-cdn.com/content/v1/631349e6c645d416c5f05d60/e5d10360-aaf6-4aa6-bb80-a53ecdcbd2b4/4.png?format=750w',
             title:'STRATEGY',
             paragraph:'Create homes with sophisticated, modern designs and processes. We leverage materials and products that have demonstrated energy efficiency and durability.',
-        }
+        },
     ]
 
     return(
-        <div className="cards">
+        <div className="aboutcards">
          {about.map(post => (
             <div key={post.id}>
                 <img src={post.photo}/>
-                <h3>{post.title}</h3>
+                <h2>{post.title}</h2>
                 <p>{post.paragraph}</p>
             </div>
         ))}
--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -12,6 +12,17 @@ function About(){
             <hr/>
           </div>
           <AboutCard></AboutCard>
+          <div className="description-about">
+            <img src="https://images.squarespace-cdn.com/content/v1/631349e6c645d416c5f05d60/c3275070-4b71-4551-a0d4-7a3162726d68/image-asset.png?format=1000w"/>
+            <div className='paragraph'>
+              <h1>Delivering Impact for all Stakeholders</h1>
+              <p>We are pioneering a new way of thinking and design with high quality, efficient architecture and materials that maximizes natural light, outdoor space and energy efficiency.<br/>We are at the intersection of technology, climate sensitive real estate and resilient investing. Our proprietary algorithm is unique to the industry, informing our due-diligence process.</p>
+            </div>
+          </div>
+          <div className='paragraph'>
+              <h1>Why does this matter?</h1>
+              <p>The construction industry plays a significant role on the environment, generating 30% of total greenhouse emissions and utilizing 32% of the world’s natural resources. <br/>We believe that only ESG-sensitive businesses will thrive within the construction industry and can positively impact our environment in the years to come.</p>
+          </div>
 
         </div>
     
