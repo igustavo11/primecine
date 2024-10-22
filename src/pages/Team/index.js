8f2b9d320ff74da62c27fec05de280a86801e695
Gustavo <gustavobrun1224@gmail.com>
2024-10-22 11:15:45 -0300

    frame motion

--- src/pages/Team/index.js
+++ src/pages/Team/index.js
@@ -1,25 +1,31 @@
-
 import './team.css';
 import TeamCard from '../../components/TeamCard';
+import { motion } from 'framer-motion';
 
-function Team(){
-
-    return(
+function Team() {
+    return (
         <>
-         <div className="home-container">
-                    <div className="content-center">
-                        <h3 className="home-title">Welcome to Prosperium</h3>
-                        <p>aaaafafaafafafafafafafafa</p>
-                    </div>
+            <div className="home-container">
+                <div className="content-center">
+                    <h3 className="home-title">Welcome to Prosperium</h3>
+                    <p>aaaafafaafafafafafafafafa</p>
                 </div>
+            </div>
+        <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 20 }}
+            transition={{ duration: 0.3 }}
+        >
+        
 
-        <div className="Team">
-            <TeamCard/>
-        </div>      
-        
+            <div className="Team">
+                <TeamCard />
+            </div>
+        </motion.div>
+
         </>
-        
-    )
+    );
 }
- 
- export default Team;
+
+export default Team;
