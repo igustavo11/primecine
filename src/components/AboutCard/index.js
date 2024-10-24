d81ec911aa7c311cd0a0844987b721ff22d33fcb
Gustavo <gustavobrun1224@gmail.com>
2024-10-24 14:16:41 -0300

    styles about frame motion

--- src/components/AboutCard/index.js
+++ src/components/AboutCard/index.js
@@ -1,57 +1,73 @@
 import './aboutcard.css';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/swiper-bundle.css'; 
-import { Pagination} from 'swiper/modules';
+import { Pagination } from 'swiper/modules';
 import 'swiper/css/pagination';
+import { motion } from 'framer-motion';
 
-
-function AboutCard(){
+function AboutCard() {
     const about = [
         {
             id: 1,
-            photo:'https://images.squarespace-cdn.com/content/v1/631349e6c645d416c5f05d60/eeabbe03-e99b-4aac-bb15-7cc65ad61e69/3.png?format=750w',
-            title:'MISSION',
-            paragraph:'Generate attractive returns on equity for our partners and investors based on asset appreciation, all with a mindset of climate awareness through efficiency.',
+            photo: 'https://images.squarespace-cdn.com/content/v1/631349e6c645d416c5f05d60/eeabbe03-e99b-4aac-bb15-7cc65ad61e69/3.png?format=750w',
+            title: 'MISSION',
+            paragraph: 'Generate attractive returns on equity for our partners and investors based on asset appreciation, all with a mindset of climate awareness through efficiency.',
         },
         {
-            id:2,
-            photo:'https://images.squarespace-cdn.com/content/v1/631349e6c645d416c5f05d60/e5d10360-aaf6-4aa6-bb80-a53ecdcbd2b4/4.png?format=750w',
-            title:'STRATEGY',
-            paragraph:'Create homes with sophisticated, modern designs and processes. We leverage materials and products that have demonstrated energy efficiency and durability.',
+            id: 2,
+            photo: 'https://images.squarespace-cdn.com/content/v1/631349e6c645d416c5f05d60/e5d10360-aaf6-4aa6-bb80-a53ecdcbd2b4/4.png?format=750w',
+            title: 'STRATEGY',
+            paragraph: 'Create homes with sophisticated, modern designs and processes. We leverage materials and products that have demonstrated energy efficiency and durability.',
         },
         {
-            id:3,
-            photo:'https://images.squarespace-cdn.com/content/v1/631349e6c645d416c5f05d60/e5d10360-aaf6-4aa6-bb80-a53ecdcbd2b4/4.png?format=750w',
-            title:'STRATEGY',
-            paragraph:'Create homes with sophisticated, modern designs and processes. We leverage materials and products that have demonstrated energy efficiency and durability.',
+            id: 3,
+            photo: 'https://images.squarespace-cdn.com/content/v1/631349e6c645d416c5f05d60/e5d10360-aaf6-4aa6-bb80-a53ecdcbd2b4/4.png?format=750w',
+            title: 'STRATEGY',
+            paragraph: 'Create homes with sophisticated, modern designs and processes. We leverage materials and products that have demonstrated energy efficiency and durability.',
         },
-    ]
+    ];
 
-    return(
+    const slideAnimation = (index) => ({
+        hidden: { opacity: 0, y: 50 },
+        visible: { 
+            opacity: 1, 
+            y: 0, 
+            transition: { 
+                duration:2.2, 
+                delay: index * 0.3
+            } 
+        }
+    });
+
+    return (
         <Swiper
-        modules={[Pagination]}
-        spaceBetween={20}
-        slidesPerView={1} 
-        pagination={{ clickable: true }}
-        breakpoints={{
-          640: {
-            slidesPerView: 2,
-          },
-          768: {
-            slidesPerView: 3, 
-          },
-        }}>
-        <div className="aboutcards">
-         {about.map(post => (
-            <SwiperSlide key={post.id}>
-                <img src={post.photo}/>
-                <h2>{post.title}</h2>
-                <p>{post.paragraph}</p>
-            </SwiperSlide>
-        ))}
-
-        </div>
+            modules={[Pagination]}
+            spaceBetween={20}
+            slidesPerView={1}
+            pagination={{ clickable: true }}
+            breakpoints={{
+                640: { slidesPerView: 2 },
+                768: { slidesPerView: 3 },
+            }}
+        >
+            <div className="aboutcards">
+                {about.map((post, index) => (
+                    <SwiperSlide key={post.id}>
+                        <motion.div
+                            initial="hidden"
+                            whileInView="visible"
+                            viewport={{ once: true }}
+                            variants={slideAnimation(index)}
+                        >
+                            <img src={post.photo} alt={post.title} />
+                            <h2>{post.title}</h2>
+                            <p>{post.paragraph}</p>
+                        </motion.div>
+                    </SwiperSlide>
+                ))}
+            </div>
         </Swiper>
     );
-};
+}
+
 export default AboutCard;
--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,111 +1,110 @@
-
 import './about.css';
 import AboutCard from '../../components/AboutCard';
+import { motion } from 'framer-motion';
 
-
-
-function About(){
+function About() {
   const description = [
     {
-        id: 1,
-        photo:'https://web.xpi.com.br/xp-new/wp-content/uploads/sites/18/2022/12/FII-bloco-1-1.png?w=1200&h=undefined&q=75',
-        title:'Sonho Grande',
-        subtitle:'Ser referência fazendo o certo da forma certa!',
-        paragraph1:'Chegamos até aqui buscando o impossível. Procuramos desafios além das nossas capacidades e não nos conformamos com o status quo.',
-        paragraph2: 'Seremos liderança global nas questões ESG, transformando problemas socioambientais em oportunidades de negócio, inspirando empresas e pessoas a construir um futuro melhor e posicionando-se como empresa que se responsabiliza pelo seu legado.',
+      id: 1,
+      photo: 'https://web.xpi.com.br/xp-new/wp-content/uploads/sites/18/2022/12/FII-bloco-1-1.png?w=1200&h=undefined&q=75',
+      title: 'Sonho Grande',
+      subtitle: 'Ser referência fazendo o certo da forma certa!',
+      paragraph1: 'Chegamos até aqui buscando o impossível. Procuramos desafios além das nossas capacidades e não nos conformamos com o status quo.',
+      paragraph2: 'Seremos liderança global nas questões ESG, transformando problemas socioambientais em oportunidades de negócio, inspirando empresas e pessoas a construir um futuro melhor e posicionando-se como empresa que se responsabiliza pelo seu legado.',
     },
     {
       id: 2,
-      photo:'https://web.xpi.com.br/xp-new/wp-content/uploads/sites/18/2022/12/FII-bloco-1-1.png?w=1200&h=undefined&q=75',
-      title:'Sonho Grande',
-      subtitle:'Ser referência fazendo o certo da forma certa!',
-      paragraph1:'Chegamos até aqui buscando o impossível. Procuramos desafios além das nossas capacidades e não nos conformamos com o status quo.',
+      photo: 'https://web.xpi.com.br/xp-new/wp-content/uploads/sites/18/2022/12/FII-bloco-1-1.png?w=1200&h=undefined&q=75',
+      title: 'Sonho Grande',
+      subtitle: 'Ser referência fazendo o certo da forma certa!',
+      paragraph1: 'Chegamos até aqui buscando o impossível. Procuramos desafios além das nossas capacidades e não nos conformamos com o status quo.',
       paragraph2: 'Seremos liderança global nas questões ESG, transformando problemas socioambientais em oportunidades de negócio, inspirando empresas e pessoas a construir um futuro melhor e posicionando-se como empresa que se responsabiliza pelo seu legado.',
     },
-    {
-      id: 3,
-      photo:'https://web.xpi.com.br/xp-new/wp-content/uploads/sites/18/2022/12/FII-bloco-1-1.png?w=1200&h=undefined&q=75',
-      title:'Sonho Grande',
-      subtitle:'Ser referência fazendo o certo da forma certa!',
-      paragraph1:'Chegamos até aqui buscando o impossível. Procuramos desafios além das nossas capacidades e não nos conformamos com o status quo.',
-      paragraph2: 'Seremos liderança global nas questões ESG, transformando problemas socioambientais em oportunidades de negócio, inspirando empresas e pessoas a construir um futuro melhor e posicionando-se como empresa que se responsabiliza pelo seu legado.',
-    },
-    {
-      id: 4,
-      photo:'https://web.xpi.com.br/xp-new/wp-content/uploads/sites/18/2022/12/FII-bloco-1-1.png?w=1200&h=undefined&q=75',
-      title:'Sonho Grande',
-      subtitle:'Ser referência fazendo o certo da forma certa!',
-      paragraph1:'Chegamos até aqui buscando o impossível. Procuramos desafios além das nossas capacidades e não nos conformamos com o status quo.',
-      paragraph2: 'Seremos liderança global nas questões ESG, transformando problemas socioambientais em oportunidades de negócio, inspirando empresas e pessoas a construir um futuro melhor e posicionando-se como empresa que se responsabiliza pelo seu legado.',
-    },
-    ]
+  ];
 
-    const Id1 = description.find(item => item.id === 1);
-    const Id2 = description.find(item => item.id === 2);
-    const Id3 = description.find(item => item.id === 3);
-    const Id4 = description.find(item => item.id === 4);
+  
+  const leftAnimation = {
+    hidden: { opacity: 0, x: -100 },
+    visible: { opacity: 1, x: 0, transition: { duration: 1.3 } }
+  };
 
-    return(
-        <>
 
-        <div className="home-container">
-                    <div className="content-center">
-                        <h3 className="home-title">Our passion for real estate is </h3>
-                        <h3 className="home-title">the secret to your sucess</h3>
-                    </div>
-                </div>
+  const rightAnimation = {
+    hidden: { opacity: 0, x: 100 },
+    visible: { opacity: 1, x: 0, transition: { duration: 1.3 } }
+  };
 
-          <section className="transition-paragraph">
-            <h1>Nossos Pilares</h1>
-            <AboutCard></AboutCard>
-          </section>
-          
-          <section className="description-about1">
-            <div className='paragraph'>
-              <h1>{Id1.title}</h1>
-              <h3>{Id1.subtitle}</h3>
-              <p>{Id1.paragraph1}<br/>{Id1.paragraph2}</p>
-            </div>
-            <img src={Id1.photo}/>
-          </section>
+  const Id1 = description.find(item => item.id === 1);
+  const Id2 = description.find(item => item.id === 2);
 
-          <section className="description-about2">
-            <img src={Id2.photo}/>
-            <div className='paragraph'>
-              <h1>{Id2.title}</h1>
-              <h3>{Id2.subtitle}</h3>
-              <p>{Id2.paragraph1}<br/>{Id2.paragraph2}</p>
-            </div>
-          </section>
+  return (
+    <>
+      <div className="home-container">
+        <div className="content-center">
+          <h3 className="home-title">Our passion for real estate is</h3>
+          <h3 className="home-title">the secret to your success</h3>
+        </div>
+      </div>
 
-          <section className="description-about1">
-            <div className='paragraph'>
-              <h1>{Id3.title}</h1>
-              <h3>{Id3.subtitle}</h3>
-              <p>{Id3.paragraph1}<br/>{Id3.paragraph2}</p>
-            </div>
-            <img src={Id3.photo}/>
-          </section>
+      <section className="transition-paragraph">
+        <h1>Nossos Pilares</h1>
+        <AboutCard />
+      </section>
 
-          <section className="description-about2">
-            <img src={Id4.photo}/>
-            <div className='paragraph'>
-              <h1>{Id4.title}</h1>
-              <h3>{Id4.subtitle}</h3>
-              <p>{Id4.paragraph1}<br/>{Id4.paragraph2}</p>
-            </div>
-          </section>
+      <motion.section
+        className="description-about1"
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.5 }} 
+        variants={leftAnimation}
+      >
+        <div className="paragraph">
+          <h1>{Id1.title}</h1>
+          <h3>{Id1.subtitle}</h3>
+          <p>{Id1.paragraph1}<br />{Id1.paragraph2}</p>
+        </div>
+        <img src={Id1.photo} alt={Id1.title} />
+      </motion.section>
 
-          <section className="values">
-            <h1>VALUES</h1>
-            <p>Our core values are deeply ingrained principles that guide all of Shape Equity’s actions; they serve as our cultural cornerstones. We believe these are crucial for the success of the business, all stakeholders and the communities we are a part of.<br/>Accountability   •   Trust   •   Integrity   •   Excellence   •   Teamwork</p>
-          </section>
+      <motion.section
+        className="description-about2"
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.5 }}
+        variants={rightAnimation}
+      >
+        <img src={Id2.photo} alt={Id2.title} />
+        <div className="paragraph">
+          <h1>{Id2.title}</h1>
+          <h3>{Id2.subtitle}</h3>
+          <p>{Id2.paragraph1}<br />{Id2.paragraph2}</p>
+        </div>
+      </motion.section>
+      
+      <motion.section
+        className="description-about1"
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.5 }} 
+        variants={leftAnimation}
+      >
+        <div className="paragraph">
+          <h1>{Id1.title}</h1>
+          <h3>{Id1.subtitle}</h3>
+          <p>{Id1.paragraph1}<br />{Id1.paragraph2}</p>
+        </div>
+        <img src={Id1.photo} alt={Id1.title} />
+      </motion.section>
 
-        </>
-    
-    )
+
+      
+
+      <section className="values">
+        <h1>VALUES</h1>
+        <p>Our core values are deeply ingrained principles that guide all of Shape Equity’s actions; they serve as our cultural cornerstones. We believe these are crucial for the success of the business, all stakeholders, and the communities we are a part of.<br />Accountability • Trust • Integrity • Excellence • Teamwork</p>
+      </section>
+    </>
+  );
 }
-
-
 
 export default About;
