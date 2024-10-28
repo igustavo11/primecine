0f5c7fed36df50b458a924b4182cfd2039037195
Fernanda <fernanvipo7@gmail.com>
2024-10-28 20:38:08 -0300

    animações no aboutdescription

--- src/components/AboutDescription/index.js
+++ src/components/AboutDescription/index.js
@@ -1,4 +1,5 @@
 import './aboutdescription.css';
+import { motion } from 'framer-motion';
 
 function AboutDescription(){
     const description = [
@@ -20,18 +21,34 @@ function AboutDescription(){
         },
     ];
 
+    const leftAnimation = {
+      hidden: { opacity: 0, x: -100 },
+      visible: { opacity: 1, x: 0, transition: { duration: 1.3 } }
+    };
+  
+  
+    const rightAnimation = {
+      hidden: { opacity: 0, x: 100 },
+      visible: { opacity: 1, x: 0, transition: { duration: 1.3 } }
+    };
     return(
         <div className='description-about'>
-             {description.map((post, index) => (
-            <div key={index} 
-            className={`post ${index % 2 === 0 ? 'left' : 'right'} ${index % 2 === 0 ? 'default-bg' : 'green-bg'}`}>
-            <div className="paragraph">
-                <h1>{post.title}</h1>
-                <h3>{post.subtitle}</h3>
-                <p>{post.paragraph1}<br />{post.paragraph2}</p>
-             </div>
-             <img src={post.photo} alt={post.title} />
-             </div>
+            {description.map((post, index) => (
+            <motion.div
+              key={index}
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true, amount: 0.5 }} 
+              variants={index % 2 === 0 ? leftAnimation : rightAnimation}
+              className={`post ${index % 2 === 0 ? 'left' : 'right'} ${index % 2 === 0 ? 'default-bg' : 'green-bg'}`}
+            >
+              <div className="paragraph">
+                  <h1>{post.title}</h1>
+                  <h3>{post.subtitle}</h3>
+                  <p>{post.paragraph1}<br />{post.paragraph2}</p>
+              </div>
+              <img src={post.photo} alt={post.title} />
+             </motion.div>
              ))}
         </div>
     
