5a57c057014e79169826ba12fb9c4e3554fe0392
igustavo11 <131629449+igustavo11@users.noreply.github.com>
2024-11-11 21:38:07 -0300

    home testing

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,7 @@
 import './home.css';
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
-import napkinSelection from '../../napkin-selection.png'; 
+import cardH from '../../casa.png'
 import HomeCard from '../../components/HomeCard';
 
 function Home() {
@@ -19,7 +19,7 @@ function Home() {
             <section className="section">
                 <div className="container">
                     <div className="icon">
-                        <img src="https://images.squarespace-cdn.com/content/v1/631349e6c645d416c5f05d60/f1f178da-65cf-4217-be39-5d6176a1d421/10.png?format=500w" alt="Icon" />
+                        <img src={cardH} alt="Icon" />
                     </div>
                     <div className="content">
                         <h2 className="title">{t('prosperiumTitle')}</h2>
